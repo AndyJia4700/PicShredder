3bcc8bba5bccd2b1d82af3ba8d407a927e73dfb5
andy <jia470079834@gmail.com>
2020-06-26 13:37:33 -0400

    try to commit

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -3,16 +3,37 @@ class canvas{
     constructor(){
         this.canvas = document.getElementById('image');
         this.ctx = this.canvas.getContext('2d');
-        this.img = document.getElementById('parent-img');
-        this.ctx.drawImage(this.img, 0, 0, 500, 500);
-        this.clickCropImage();
-        // this.restore = this.restore.bind(this);
+        this.chooseImage();
+    }
+
+    chooseImage(){
+        const img = document.getElementById('parent-img');
+        img.addEventListener('change', function(){
+            localStorage.setItem('setImg', this.src);
+        });
+        let src = localStorage.getItem('setImg');
+        if (src) img.src = src;
+        
+        this.ctx.drawImage(img, 0, 0, 500, 500);
+        if (img.src) {
+            this.chooseGrid();
+        }
+    }
+
+    chooseGrid(){
+        const grid = document.getElementById("grid");
+        grid.addEventListener("change", function () {
+            localStorage.setItem("selValue", this.value);
+        });
+
+        let val = localStorage.getItem("selValue");
+        if (val) grid.value = val;
+
+        this.clickCropImage(val);
     }
 
     
-    clickCropImage(){
-        // debugger;
-        const grid = 12;
+    clickCropImage(grid){
         const dalta = 0;
         const canvasH = this.canvas.clientHeight;
         const canvasW = this.canvas.clientWidth;
@@ -21,7 +42,8 @@ class canvas{
         
         const ul = document.createElement('ul')
         ul.id = "child-img-ul"
-        document.body.append(ul);
+        // document.body.append(ul);
+        document.getElementById("dashboard").append(ul);
         
         for (let x = 0; x < grid; x++) {
             for (let y = 0; y < grid; y++) {
@@ -29,18 +51,64 @@ class canvas{
                 i++;
             }
         } 
-        this.shuffle();
 
+        this.buttons();
+        this.shuffle();
+        document.getElementById("shuffle-btn").addEventListener("click", () => {
+            location.reload();
+            return false;
+        });
+        this.clicks();
+    }
+
+    buttons(){
+
+        const qbtn = document.createElement("BUTTON");
+        qbtn.innerHTML = "QUICK SORT";
+        qbtn.id = "quick-btn";
+        qbtn.className="sort-btns";
+        document.getElementById("sort-btns").appendChild(qbtn);
+
+        const bbtn = document.createElement("BUTTON");
+        bbtn.innerHTML = "BUBBLE SORT";
+        bbtn.id = "bubble-btn";
+        bbtn.className = "sort-btns";
+        document.getElementById("sort-btns").appendChild(bbtn);
+
+        const mbtn = document.createElement("BUTTON");
+        mbtn.innerHTML = "MERGE SORT";
+        mbtn.id = "merge-btn";
+        mbtn.className = "sort-btns";
+        document.getElementById("sort-btns").appendChild(mbtn);
+    }
+
+    clicks(){
         let arr = [];
         let shuffled = [];
         shuffled = document.querySelectorAll("img");
         for (let idx = 1; idx < shuffled.length; idx++) {
-            arr = arr.concat([[shuffled[idx].className, shuffled[idx]]])
-            // document.body.append(shuffled[idx]);
-        }        
-        this.quickSort(arr); 
+          arr = arr.concat([[shuffled[idx].className, shuffled[idx]]]);
+        }
+
+        document.getElementById("quick-btn").addEventListener("click", () => {
+          this.quickSort(arr);
+        });
+
+        document.getElementById("bubble-btn").addEventListener("click", () => {
+          this.bubbleSort(arr);
+        });
+
+        document.getElementById("merge-btn").addEventListener("click", () => {
+          this.mergeSort(arr);
+        });
     }
 
+
+
+    callback(x, y) {
+       if(x < y) return -1;
+        return 1;
+    }
 
     quickSort(arr){
         if (arr.length < 2) return arr;
@@ -54,22 +122,70 @@ class canvas{
         this.restore(sorted);
         return sorted
     }
-    
+
+    bubbleSort(arr){
+        if (arr.length < 2) return arr;
+
+        let sorted = false;
+        while (!sorted) {
+          sorted = true;
+          for (let i = 0; i < arr.length - 1; i++) {
+            if (this.callback( parseInt(arr[i]), parseInt(arr[i + 1]) ) === 1) {
+              sorted = false;
+              [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+            }
+          }
+        }
+        this.restore(arr);
+        return arr;
+    }
+
+    mergeSort(arr){
+        if (arr.length < 2) return arr;
+        const midpoint = Math.floor(arr.length/2);
+        const left = this.mergeSort(arr.slice(0, midpoint));
+        const right = this.mergeSort(arr.slice(midpoint));
+
+        return this.merge(left, right);
+    }
+
+    merge(left, right){
+        let merged = [];
+        while (left.length && right.length){
+            switch (this.callback( parseInt(left[0][0]), parseInt(right[0][0]) )) {
+                case -1:
+                    merged.push(left.shift());
+                    break;
+                case 1:
+                    merged.push(right.shift());
+                    break;
+            }
+        }
+        merged = merged.concat(left, right);
+        this.restore(merged);
+        return merged;
+    }
+
     
     restore(arr){
+
+        // const speed = document.getElementById("speed");
+        // speed.addEventListener("change", function () {
+        //     localStorage.setItem("selRate", this.value);
+        // });
+        // let rate = localStorage.getItem("selRate");
+        // if (rate) speed.value = rate;
+
+        let rate = document.getElementById("speed").value;
+
         for (let idx = 0; idx < arr.length; idx++) {
             (function (ind) {
                 setTimeout(function () {
                     const sortImg = document.getElementById("child-img-ul");                    
-                    sortImg.append(arr[idx][1]);              
-                }, 100 + 100 * ind);
+                    sortImg.append(arr[idx][1]);
+                }, 100 + rate * ind);
             })(idx);
         }
-    }
-
-    callback(x, y) {
-       if(x < y) return -1;
-        return 1;
     }
 
     shuffle(){
